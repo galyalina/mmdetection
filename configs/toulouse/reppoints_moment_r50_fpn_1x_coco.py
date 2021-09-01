bc5c01229e31f66737e8272cbb7dbe43d395d50a
Galya Litkin <glitkin@gmail.com>
2021-09-01 15:41:06 +0200

    Fix numebr of classes

--- configs/toulouse/reppoints_moment_r50_fpn_1x_coco.py
+++ configs/toulouse/reppoints_moment_r50_fpn_1x_coco.py
@@ -1,12 +1,6 @@
 # The new config inherits a base config to highlight the necessary modification
 _base_ = 'reppoints/reppoints_moment_r50_fpn_1x_coco.py'
 
-# We also need to change the num_classes in head to match the dataset's annotation
-model = dict(
-    roi_head=dict(
-        bbox_head=dict(num_classes=1),
-        mask_head=dict(num_classes=1)))
-
 # Modify dataset related settings
 dataset_type = 'COCODataset'
 classes = ('building',)
