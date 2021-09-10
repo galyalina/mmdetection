fc6e09e66c2bca62a3f7f57b752e5fb3e68c837c
Galya Litkin <glitkin@gmail.com>
2021-09-10 16:44:49 +0200

    Change number of classes to simgle buildings

--- configs/_base_/datasets/toulouse_detection.py
+++ configs/_base_/datasets/toulouse_detection.py
@@ -1,7 +1,7 @@
 # dataset settings
 dataset_type = 'CocoDataset'
 data_root = 'data/toulouse/'
-classes = ('building')
+classes = ('building',)
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
