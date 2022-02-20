73842b9f8480b994980a782b77e34f85548302e1
Galya Litkin <glitkin@gmail.com>
2022-02-20 16:06:51 +0100

    Move toulouse configs to separate folder

---
+++ configs/reppoints_toulouse/default_runtime.py
@@ -0,0 +1,17 @@
+checkpoint_config = dict(interval=1)
+# yapf:disable
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(type='TensorboardLoggerHook')
+    ])
+# yapf:enable
+custom_hooks = [dict(type='NumClassCheckHook')]
+
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+# resume_from = 'work_dirs/reppoints_moment_r101_fpn_gn-neck+head_2x_toulouse/epoch_23.pth'
+workflow = [('train', 1), ('val', 1)]
